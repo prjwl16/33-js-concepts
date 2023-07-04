400f6d10f304f597fe86e9c28d6335e0b3fac8ba
prjwl16 <prajumore76@gmail.com>
2023-07-05 00:18:09 +0530

    07

---
+++ 07-expession-vs-statement/07-expression-vs-statement.js
@@ -0,0 +1,16 @@
+// An expression is a piece of code that produces a value.
+// It can be as simple as a literal value (e.g., 5, "Hello"), a variable, or a more complex combination of operators and operands.
+// For example:
+2 + 3;
+
+// A statement is a piece of code that performs some action.
+// For example:
+console.log(2 + 3);
+
+//All
+
+var x = 5; // Variable declaration and assignment statement
+if (x > 3) {
+  // If statement
+  console.log("x is greater than 3");
+}
