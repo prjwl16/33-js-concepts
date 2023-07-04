0ce9162ae99112ec9bb692227406a1eedc6bcad7
prjwl16 <prajumore76@gmail.com>
2023-07-05 02:52:06 +0530

    02

---
+++ 02-primitive-types/02-primitive-types.js
@@ -0,0 +1,24 @@
+//there are 7 primitive types in JS
+// 1. number
+// 2. string
+// 3. boolean
+// 4. null
+// 5. undefined
+// 6. symbol
+// 7. bigint
+
+//Examples of primitive types
+//number
+let age = 30;
+//string
+let name = "John";
+//boolean
+let isMarried = false;
+//null
+let car = null;
+//undefined
+let job;
+//symbol
+let sym = Symbol();
+//bigint
+let bigInt = 1234567890123456789012345678901234567890n;
